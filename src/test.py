eb07b6b3ae32600c35102a35e61abc574b1a6fa2
victorvianna <victor.vianna10@gmail.com>
2019-11-20 22:36:16 +0100

    Separate tests and exception test

--- src/test.py
+++ src/test.py
@@ -7,7 +7,13 @@ for grammar in grammars.values():
     init_grammar(grammar)
     for rule in grammar.values():
         for n in range(0, N_MAX):
-            assert rule.list(n) == [rule.unrank(n, i) for i in range(0, rule.count(n))]
+            assert len(rule.list(n)) == rule.count(n)
+            assert all([rule.unrank(n, i) == obj for i, obj in enumerate(rule.list(n))])
             assert all([len(el) == n for el in rule.list(n)])
             assert all([rule.rank(obj) == i for i, obj in enumerate(rule.list(n))])
-
+            try:
+                rule.unrank(n, rule.count(n) + 1)
+            except:
+                continue
+            # Not reached if unrank threw exception for index bigger than count.
+            assert False
